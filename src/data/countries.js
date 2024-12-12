db9e3c26badbd9957baaf3dd180bda75f34e9f81
kareemAmin233 <aminmkareem11@gmail.com>
2024-12-12 22:01:41 +0200

    Add files via upload

---
+++ src/data/countries.js
@@ -0,0 +1,138 @@
+
+const countries = [
+  {
+    id: "1",
+    name: ["الجزائر", "الجزائر"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Algeria.json`,
+    color: "#DC143C",
+  },
+  {
+    id: "2",
+    name: ["البحرين"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Bahrain.json`,
+    color: "#6495ED",
+  },
+  {
+    id: "3",
+    name: ["جزر القمر"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Comoros.json`,
+    color: "#32CD32",
+  },
+  {
+    id: "4",
+    name: ["جيبوتي"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Djibouti.json`,
+    color: "#DAA520",
+  },
+  {
+    id: "5",
+    name: ["مصر"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Egypte.json`,
+    color: "#DA70D6",
+  },
+  {
+    id: "6",
+    name: ["الإمارات", "الامارات"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Emirates.json`,
+    color: "#FF6347",
+  },
+  {
+    id: "7",
+    name: ["العراق"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Irak.json`,
+    color: "#708090",
+  },
+  {
+    id: "8",
+    name: ["الاردن", "الأردن"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Jordan.json`,
+    color: "#00BFFF",
+  },
+  {
+    id: "9",
+    name: ["الكويت"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Kuwait.json`,
+    color: "#3CB371",
+  },
+  {
+    id: "10",
+    name: ["لبنان"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Lebanon.json`,
+    color: "#FF7F50",
+  },
+  {
+    id: "11",
+    name: ["ليبيا"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Libya.json`,
+    color: "#9932CC",
+  },
+  {
+    id: "12",
+    name: ["المغرب"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Maroc.json`,
+    color: "#FF8C00",
+  },
+  {
+    id: "13",
+    name: ["موريتانيا"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Mauritania.json`,
+    color: "#DB7093",
+  },
+  {
+    id: "14",
+    name: ["عمان"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Oman.json`,
+    color: "#4682B4",
+  },
+  {
+    id: "15",
+    name: ["قطر"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Qatar.json`,
+    color: "#00FF7F",
+  },
+  {
+    id: "16",
+    name: ["السعودية"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Saoedi.json`,
+    color: "#9370DB",
+  },
+  {
+    id: "17",
+    name: ["الصومال"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Somali.json`,
+    color: "#D2691E",
+  },
+  {
+    id: "18",
+    name: ["السودان"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Sudan.json`,
+    color: "#CD5C5C",
+  },
+  {
+    id: "19",
+    name: ["سوريا"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Syria.json`,
+    color: "#20B2AA",
+  },
+  {
+    id: "20",
+    name: ["تونس"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Tunisia.json`,
+    color: "#BDB76B",
+  },
+
+  {
+    id: "21",
+    name: ["فلسطين"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Palestine.json`,
+    color: "#F4A460",
+  },
+  {
+    id: "22",
+    name: ["اليمن", "يمن"],
+    path: `${import.meta.env.BASE_URL}/countriesBoundary/Yemen.json`,
+    color: "#FF69B4",
+  },
+];
+
+export default countries;
---
+++ vite.config.js
@@ -0,0 +1,62 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      // registerType: "autoUpdate",
+      // devOptions: {
+      //   enabled: true,
+      // },
+      registerType: "prompt",
+      includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: " تحدي   الدول العربية",
+        short_name: "الدول العربية",
+        description: "تحدي الدول العربية ",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "apple touch icon",
+          },
+          {
+            src: "/maskable_icon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/maskable_icon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        theme_color: "#ddd",
+        background_color: "#cecece",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+      },
+    }),
+  ],
+  base: "/arab-countries-maps/",
+});
+
